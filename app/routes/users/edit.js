879b02843796c361a280b95c0545d2e330e6569c
Alex Espinoza <aespinoza@aspria.net>
2014-05-15 16:23:11 -0400

    Create save action in user edit route

--- app/routes/users/edit.js
+++ app/routes/users/edit.js
@@ -1,5 +1,12 @@
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find('user', params.user_id);
+  },
+
+  actions: {
+    save: function(model) {
+      model.save();
+      this.transitionTo('users.show', model);
+    }
   }
 });
