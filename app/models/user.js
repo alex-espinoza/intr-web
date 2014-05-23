42842c1831f739ff2d86a15e5213b3784037edce
Alex Espinoza <aespinoza@aspria.net>
2014-05-22 21:12:22 -0400

    Get test users data from rails API

--- app/models/user.js
+++ app/models/user.js
@@ -2,12 +2,16 @@ var User = DS.Model.extend({
   email: DS.attr('string')
 });
 
-User.reopenClass({
-  FIXTURES: [
-    { id: 1, email: "test1@cool.com" },
-    { id: 2, email: "test2@wow.com" },
-    { id: 3, email: "test3@nice.com" }
-  ]
+// User.reopenClass({
+//   FIXTURES: [
+//     { id: 1, email: "test1@cool.com" },
+//     { id: 2, email: "test2@wow.com" },
+//     { id: 3, email: "test3@nice.com" }
+//   ]
+// });
+
+DS.RESTAdapter.reopen({
+  namespace: 'api/v1'
 });
 
 export default User;
